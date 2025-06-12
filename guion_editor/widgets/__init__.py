7620e09603ce78414c96efd5223c7ed6f60b831f
StablePeru <stableperu@gmail.com>
2025-06-12 09:53:06 +0200

    refactor: Remove CustomTableWidget and update imports in __init__.py

--- guion_editor/widgets/__init__.py
+++ guion_editor/widgets/__init__.py
@@ -4,5 +4,4 @@ from .video_player_widget import VideoPlayerWidget
 from .video_window import VideoWindow
 from .table_window import TableWindow
 from .config_dialog import ConfigDialog
-from .custom_table_widget import CustomTableWidget
 from .shortcut_config_dialog import ShortcutConfigDialog
